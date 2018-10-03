26302a6f30e9ad39eede7c4c24e742c87c462b72
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-03 21:12:57 +0300

    6/12 passing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = function solveSudoku(matrix) {
   let board = matrix;
   const isSolved = (board) => {
     let check = 1;
-    for (let i = 0; i <= 8; i++)
-      for (let j = 0; j <= 8; j++) {
+    for (let i = 0; i < 9; i++)
+      for (let j = 0; j < 9; j++) {
         if (board[i][j] === 0) check = 0;
       };
     return (check);
@@ -11,9 +11,9 @@ module.exports = function solveSudoku(matrix) {
   
   const possibleVariants = (board, x, y) => {
     let variantsArray = [];
-    for (let i = 0; i <= 8; i++) variantsArray.push(0);
+    for (let i = 0; i < 9; i++) variantsArray.push(0);
 
-    for (let i = 0; i <= 8; i++) {
+    for (let i = 0; i < 9; i++) {
       if (board[x][i] !== 0) variantsArray[board[x][i] - 1] = 1;
       if (board[i][y] !== 0) variantsArray[board[i][y] - 1] = 1;
     };
@@ -21,18 +21,25 @@ module.exports = function solveSudoku(matrix) {
     let k = 0, 
         l = 0;
     
-    if ((x >= 0)&&(x <= 2)) k = 0
-      else if ((x >= 3)&&(x <= 5)) k = 3
-        else k = 5;
-    if ((y >= 0)&&(y <= 2)) l = 0
-      else if ((y >= 3)&&(y <= 5)) l = 3
-        else l = 5;
+    if ((x >= 0)&&(x <= 2)) 
+      k = 0
+    else if ((x >= 3)&&(x <= 5)) 
+      k = 3
+    else 
+      k = 6;
+    
+    if ((y >= 0)&&(y <= 2)) 
+      l = 0
+    else if ((y >= 3)&&(y <= 5)) 
+      l = 3
+    else 
+      l = 6;
 
     for (let i = k; i !== k+2; i++)
       for (let j = l; j !== l+2; j++) {
         if (board[i][j] !== 0) variantsArray[board[i][j] - 1] = 1;
       };
-    for (let i = 0; i <= 8; i++) {
+    for (let i = 0; i < 9; i++) {
       if (variantsArray[i] === 0) variantsArray[i] = i+1
       else variantsArray[i] = 0;
     }
@@ -44,34 +51,31 @@ module.exports = function solveSudoku(matrix) {
         y = 0,
         variantsArray = [];
 
-      
     if (isSolved(board) === 1) {
-      console.log(board);
       return board;
-    }
-    else {
-      for (let i = 0; i <= 8; i++)
-        for (let j = 0; j <= 8; j++) {
+    };
+    if (isSolved(board) === 0) {
+      loop: for (let i = 0; i < 9; i++)
+        for (let j = 0; j <  9; j++)
           if (board[i][j] === 0) {
             x = i;
             y = j;
-            break;
+            break loop;
           };
-        };
-      
       variantsArray = possibleVariants(board, x, y);
-      for (let i = 0; i <= 8; i++) {
-        if (variantsArray[i] !== 0) {
-          board[x][y] = variantsArray[i];
-          sudokuSolver(board);
+      let empty = true;
+        for (let i = 0; i < 9; i++) {
+          if (variantsArray[i] !== 0) {
+            empty = false;
+            board[x][y] = variantsArray[i];
+            variantsArray[i] = 0;
+            let res = sudokuSolver(board);
+            if (res) return res;
+          };
+          board[x][y] = 0;
         };
-        board[x][y] = 0;
-      };
+        if (empty) return false;
     };
   };
-  
-  sudokuSolver(board);
-  console.log(board);
-  return board;
+  return sudokuSolver(board);
 };
-
